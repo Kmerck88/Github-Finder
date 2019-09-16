946325e2e63900fe53f7e38658c5d7972cb7c92d
Kmerck88 <39713216+Kmerck88@users.noreply.github.com>
2019-09-16 19:17:37 -0400

    Clear users of state

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class App extends Component {
     loading: false
   };
 
-// Search Github users  
-  searchUsers= async (text) => {
-    this.setState({loading: true}); 
+  // Search Github users
+  searchUsers = async text => {
+    this.setState({ loading: true });
 
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=$
@@ -22,15 +22,24 @@ class App extends Component {
     );
 
     this.setState({ users: res.data.items, loading: false });
-}
-  
+  };
+
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
+    const {users, loading }=this.state
+    
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,41 +1,51 @@
 import React, { Component } from 'react';
-import ProTypes from 'prop-types'
+import ProTypes from 'prop-types';
 
 export class Search extends Component {
-  state={ 
+  state = {
     text: ''
   };
 
-  static ProTypes = { 
-    searchUsers: ProTypes.func.isRequired
+  static ProTypes = {
+    searchUsers: ProTypes.func.isRequired,
+    clearUsers: ProTypes.func.isRequired,
+    showClear: ProTypes.bool.isRequired
   };
 
-  onSubmit = e => { 
-  e.preventDefault();
- this.props.searchUsers(this.state.text);
- this.setState({ text: ''});
-};
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
-
   render() {
+    const {showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
-          <input 
-          type='text' 
-          name='text' 
-          placeholder='Search Users..' 
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users..'
             value={this.state.text}
             onChange={this.onChange}
           />
-         <input
+          <input
             type='submit'
             value='Search'
             className='btn btn-dark btn-block'
           />
         </form>
+        {showClear && (
+          <button
+            className='btn btn-light btn-block'
+            onClick={clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
