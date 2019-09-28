14b9a1d4958529cd56f174bfa0d61dd6b676230d
Kmerck88 <39713216+Kmerck88@users.noreply.github.com>
2019-09-28 08:15:25 -0400

    Repos & ReoItem Component & Data

--- src/App.js
+++ src/App.js
@@ -14,15 +14,14 @@ import './App.css';
  
 class App extends Component {
   state = {
+   users: [],
     user: {},
-    users: [],
+    repos: [],
     loading: false,
-    alert: null
-  }
+    alert: null,
+  };
  
-  
- 
-  //  Search Github Users
+    //  Search Github Users
   searchUsers = async (text) => {
     this.setState({ loading: true })
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
@@ -38,7 +37,19 @@ class App extends Component {
       )
     
     this.setState({ user: res.data, loading: false })
-  }
+  };
+
+  //Get users repos 
+  getUserRepos = async username => {
+    this.setState({ loading: true })
+ 
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+      {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      )
+    
+    this.setState({ repos: res.data, loading: false })
+  };
  
   // Clear Github Users
   clearUsers = () => {
@@ -53,7 +64,7 @@ class App extends Component {
   }
  
   render() {
-    const { users, user, loading } = this.state
+    const { users, user, loading,repos} = this.state
  
     return (
       <Router>
@@ -78,7 +89,9 @@ class App extends Component {
                 <User 
                   {...props} 
                   getUser={this.getUser} 
-                  user={user} 
+                  getUserRepos={this.getUserRepos} 
+                  user={user}
+                  repos={repos} 
                   loading={loading} 
                 />
               )} />
