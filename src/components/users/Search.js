a4a7edf23464cbcdb8e3aa325d25abaccfe19c0d
Kmerck88 <39713216+Kmerck88@users.noreply.github.com>
2019-09-28 17:40:09 -0400

    Search Class to Function With useState Hook

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,41 +1,28 @@
-import React, { Component } from 'react';
+import React, {useState}from 'react';
 import PropTypes from 'prop-types';
 
-export class Search extends Component {
-  state = {
-    text: ''
-  };
-
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired, 
-    setAlert: PropTypes.func.isRequired,
-    };
-
-  onSubmit = e => {
+const Search = ({searchUsers, showClear, clearUsers,setAlert}) => { const [text, setText] = useState(''); 
+  
+ const onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === '') {
-    this.props.setAlert('Please enter something', 'light');
+    if (text === '') {
+    setAlert('Please enter something', 'light');
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: '' });
+      searchUsers(text);
+      setText('')
     }
   };
 
-  onChange = e => this.setState({ [e.target.name]: e.target.value });
-
-  render() {
-    const { showClear, clearUsers } = this.props;
+  const onChange = e => setText(e.target.value);
     return (
       <div>
-        <form onSubmit={this.onSubmit} className='form'>
+        <form onSubmit={onSubmit} className='form'>
           <input
             type='text'
             name='text'
             placeholder='Search Users..'
-            value={this.state.text}
-            onChange={this.onChange}
+            value={text}
+            onChange={onChange}
           />
           <input
             type='submit'
@@ -50,7 +37,14 @@ export class Search extends Component {
         )}
       </div>
     );
-  }
+  
 }
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired, 
+  setAlert: PropTypes.func.isRequired,
+  };
 
 export default Search;
